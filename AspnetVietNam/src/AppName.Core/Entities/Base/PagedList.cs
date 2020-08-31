4610dc9901f6973636ab8e3e51132d8fe776d52a
ngnam <nguyenvannam0411@gmail.com>
2020-08-31 23:37:45 +0700

    update Entity/Base For Core project

---
+++ AspnetVietNam/src/AppName.Core/Entities/Base/PagedList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AppName.Core.Entities.Base
+{
+    public class PagedList<T> where T : class
+    {
+        public PagedList()
+        {
+            Data = new List<T>();
+        }
+
+        #region Properties
+        /// <summary>
+        /// The data result.
+        /// </summary>
+        public List<T> Data { get; set; }
+        /// <summary>
+        /// Total items count
+        /// </summary>
+        public int TotalCount { get; set; }
+        /// <summary>
+        /// Number of items contained in each page.
+        /// </summary>
+        public int PageSize { get; set; }
+
+        #endregion
+    }
+}
--- AspnetVietNam/src/AppName.Core/Entities/Base/TEntityBase.cs
+++ AspnetVietNam/src/AppName.Core/Entities/Base/TEntityBase.cs
@@ -2,6 +2,9 @@
 {
     public interface TEntityBase<Tkey>
     {
+        /// <summary>
+        /// The Primary key of Entity
+        /// </summary>
         Tkey Id { get; }
     }
 }
