253242accc945a16299a0be71afecf25e4516308
ngnam <nguyenvannam0411@gmail.com>
2020-08-31 23:51:56 +0700

    Add Specifications for Core Project

---
+++ AspnetVietNam/src/AppName.Core/Specifications/CategoryWithProductsSpecification.cs
@@ -0,0 +1,13 @@
+﻿using AppName.Core.Entities;
+using AppName.Core.Specifications.Base;
+
+namespace AppName.Core.Specifications
+{
+    public sealed class CategoryWithProductsSpecification : BaseSpecification<Category>
+    {
+        public CategoryWithProductsSpecification(int categoryId) : base(b => b.Id == categoryId)
+        {
+            AddInclude(b => b.Products);
+        }
+    }
+}
---
+++ AspnetVietNam/src/AppName.Core/Specifications/ProductWithCategorySpecification.cs
@@ -0,0 +1,18 @@
+﻿using AppName.Core.Entities;
+using AppName.Core.Specifications.Base;
+
+namespace AppName.Core.Specifications
+{
+    public sealed class ProductWithCategorySpecification : BaseSpecification<Product>
+    {
+        public ProductWithCategorySpecification() : base(null)
+        {
+            AddInclude(b => b.Category);
+        }
+
+        public ProductWithCategorySpecification(string productName): base(b => b.ProductName.ToLower().Contains(productName.ToLower()))
+        {
+            AddInclude(b => b.Category);
+        }
+    }
+}
