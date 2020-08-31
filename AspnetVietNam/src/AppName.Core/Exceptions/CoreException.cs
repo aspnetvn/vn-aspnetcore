a6eda17a034d93a794f66b6c9f286eb68c271dd0
ngnam <nguyenvannam0411@gmail.com>
2020-08-31 09:57:50 +0700

    Add CoreException

---
+++ AspnetVietNam/src/AppName.Core/Exceptions/CoreException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppName.Core.Exceptions
+{
+    [Serializable]
+    public class CoreException : Exception
+    {
+        public CoreException() { }
+        public CoreException(string message) : base(message) { }
+        public CoreException(string message, Exception inner) : base(message, inner) { }
+        protected CoreException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
