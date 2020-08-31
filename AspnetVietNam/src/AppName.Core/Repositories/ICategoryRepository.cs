9e17f075df7e8690e9567c90aac48d4607510885
ngnam <nguyenvannam0411@gmail.com>
2020-09-01 01:23:16 +0700

    Add Repositories to Core Project

---
+++ AspnetVietNam/src/AppName.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using AppName.Core.Entities;
+using AppName.Core.Repositories.Base;
+using System.Threading.Tasks;
+
+namespace AppName.Core.Repositories
+{
+    public interface ICategoryRepository : IBaseRepository<Category>
+    {
+        Task<Category> GetCategoryWithProductsAsync(int categoryId);
+    }
+}
---
+++ AspnetVietNam/src/AppName.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using AppName.Core.Entities;
+using AppName.Core.Repositories.Base;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppName.Core.Repositories
+{
+    public interface IProductRepository : IBaseRepository<Product>
+    {
+        Task<IQueryable<Product>> GetProductListAsync();
+        Task<IQueryable<Product>> GetProductByNameAsync(string productName);
+        Task<IQueryable<Product>> GetProductByCategoryAsync(int categoryId);
+    }
+}
