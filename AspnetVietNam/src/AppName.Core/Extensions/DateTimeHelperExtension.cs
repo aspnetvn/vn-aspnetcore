b7bf77dbbb5ef83a772d07a5c8b23ebbcd0b429d
ngnam <nguyenvannam0411@gmail.com>
2020-08-31 23:41:11 +0700

    Add DateTimeHelperExtension

---
+++ AspnetVietNam/src/AppName.Core/Extensions/DateTimeHelperExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AppName.Core.Extensions
+{
+    public static class DateTimeHelperExtension
+    {
+        public static DateTime FromDateTimeOffset(this DateTimeOffset utcTimeOffset)
+        {
+            return utcTimeOffset.DateTime;
+        }
+
+        public static DateTimeOffset ToDateTimeOffset(this DateTime utcTime)
+        {
+            return DateTime.SpecifyKind(utcTime, DateTimeKind.Utc);
+        }
+    }
+}
