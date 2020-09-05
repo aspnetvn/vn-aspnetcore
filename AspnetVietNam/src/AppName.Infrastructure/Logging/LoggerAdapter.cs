4bd1156118109d517880d8b09b68637d7abd69a6
ngnam <nguyenvannam0411@gmail.com>
2020-09-05 20:52:51 +0700

    Implement LoggerAdapter

---
+++ AspnetVietNam/src/AppName.Infrastructure/Logging/LoggerAdapter.cs
@@ -0,0 +1,29 @@
+﻿using AppName.Core.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace AppName.Infrastructure.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+        public LoggerAdapter(ILoggerFactory logger)
+        {
+            _logger = logger.CreateLogger<T>();
+
+        }
+        public void LogError(string message, params object[] args)
+        {
+            _logger.LogError(message, args);
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+    }
+}
